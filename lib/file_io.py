844b7bb30bec448ec2134a1ebd82bb78de65b314
horaceo1b <horaceken0@gmail.com>
2025-09-03 00:20:51 +0300

    initial commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
 def write_file(file_name, file_content):
-    pass
+    """
+    Creates or overwrites a .txt file with the given content.
+    """
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Appends content to the given .txt file (no extra newline).
+    """
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """
+    Reads and returns the contents of the given .txt file.
+    """
+    with open(f"{file_name}.txt", "r") as f:
+        return f.read()
